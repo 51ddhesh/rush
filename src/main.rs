fc01dea040b7f9e843a061c72d70aa0833a3329f
siddhesh <siddheshbadnapurkar@gmail.com>
2025-05-15 14:42:50 +0530

    added exit status and an arrow prompt

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,26 @@ extern crate libc;
 use std::env;
 use std::path::Path;
 use std::process::Command;
-use std::io;
+use std::io::{self, Write};
+use std::os::unix::process::CommandExt;
 pub mod colors;
 
 fn main() {
+
+    unsafe {
+        libc::signal(libc::SIGINT, libc::SIG_IGN);
+        libc::signal(libc::SIGQUIT, libc::SIG_IGN);
+    }
+
+    let mut last_exit_status = true;
+
     loop {
-        print_prompt();
+        print_prompt(last_exit_status);
         let mut command_string = String::new();
+        io::stdout()
+            .flush()
+            .expect("Failed to flush the buffer");
+
         io::stdin()
             .read_line(&mut command_string)
             .expect("Failed to read");
@@ -22,38 +35,68 @@ fn main() {
             }
             match command_tokens[0] {
                 "exit" => std::process::exit(0),
-                "cd" => change_dir(command_tokens[1]),  
-                _ => execute_command(command_tokens, is_background),
+                "cd" => {
+                    last_exit_status = change_dir(command_tokens[1]);
+                }  
+                _ => {
+                    last_exit_status = execute_command(command_tokens, is_background);
+                }
             }
         }
     }
 }
 
-fn execute_command(tokens: Vec<&str>, is_background: bool) {
+fn execute_command(tokens: Vec<&str>, is_background: bool) -> bool {
+    if tokens.is_empty() {
+        colors::error_logger("No command entered".to_string());
+        return false;
+    }
     let mut instance = Command::new(tokens[0]);
-    if let Ok(mut child) = instance.args(&tokens[1..]).spawn() {
+    if let Ok(mut child) = unsafe {
+        instance.args(&tokens[1..])
+            .pre_exec(|| {
+                unsafe {
+                    libc::signal(libc::SIGINT, libc::SIG_DFL);
+                    libc::signal(libc::SIGQUIT, libc::SIG_DFL);
+                }
+                Ok(())
+            })
+            .spawn()
+    } {
         if is_background == false {
-            child.wait().expect("command was not running");
+            return child.wait().expect("Command was not running").success();
         } else {
             colors::success_logger(format!("{} started", child.id()));
+            true
         }
     } else {
-        colors::error_logger("command not found".to_string());
+        colors::error_logger("Command not found".to_string());
+        false
     }
 }
 
-fn change_dir(new_path: &str) {
+fn change_dir(new_path: &str) -> bool {
     let new_path = Path::new(new_path);
     match env::set_current_dir(&new_path) {
-        Err(e)  => println!("Failed to change directory\n{}", e),
-        _ => {}
+        Err(e)  => {
+            colors::error_logger(format!("Failed to change directory\n{}", e));
+            return false;
+        },
+        _ => {},
+    }
+    return true;
+}
+
+fn print_prompt(last_exit_status: bool) {
+    let path = env::current_dir().unwrap();
+    println!("{}RUSHING IN {}{}{}", colors::ANSI_BOLD, colors::ANSI_COLOR_CYAN, path.display(), colors::RESET);
+    if last_exit_status {
+        print!("{}\u{2ba1}{}  ", colors::GREEN, colors::RESET);
+    } else {
+        print!("{}\u{2ba1}{}  ", colors::RED, colors::RESET);
     }
 }
 
-fn print_prompt() {
-    let path = env::current_dir().unwrap(); 
-    println!("{}{} >> RUSHING IN {}{}", colors::ANSI_BOLD, colors::ANSI_COLOR_CYAN, path.display(), colors::RESET);
-}
 
 fn tokenize(command_string: &mut String) -> Vec<Vec<&str>> {
     command_string.pop();
