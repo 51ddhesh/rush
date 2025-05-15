a04465c0e7a598a8c5d9d515e7d7e11ea4a188df
siddhesh <siddheshbadnapurkar@gmail.com>
2025-05-15 14:10:11 +0530

    command tokenization and colors added

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,28 @@ use std::env;
 use std::path::Path;
 use std::process::Command;
 use std::io;
+pub mod colors;
 
 fn main() {
-    println!("RUSH TERMINAL");
     loop {
-        let mut command = String::new();
+        print_prompt();
+        let mut command_string = String::new();
         io::stdin()
-            .read_line(&mut command)
-            .expect("Failed to read user command");
-        command.pop();
-        let mut command_tokens: Vec<&str> = command.split(' ').collect();
-        let mut is_background = false;
-        if let Some(&"&") = command_tokens.last() {
-            is_background = true;
-            command_tokens.pop();
-        } 
+            .read_line(&mut command_string)
+            .expect("Failed to read");
 
-        match command_tokens[0] {
-            "exit" => std::process::exit(0),
-            "cd" => change_dir(command_tokens[1]),
-            _ => execute_command(command_tokens, is_background),
+        let commands_tokens = tokenize(&mut command_string);
+        for mut command_tokens in commands_tokens {
+            let mut is_background = false;
+            if let Some(&"&") = command_tokens.last() {
+                is_background = true;
+                command_tokens.pop();
+            }
+            match command_tokens[0] {
+                "exit" => std::process::exit(0),
+                "cd" => change_dir(command_tokens[1]),  
+                _ => execute_command(command_tokens, is_background),
+            }
         }
     }
 }
@@ -33,10 +35,10 @@ fn execute_command(tokens: Vec<&str>, is_background: bool) {
         if is_background == false {
             child.wait().expect("command was not running");
         } else {
-            println!("{} started", child.id());
+            colors::success_logger(format!("{} started", child.id()));
         }
     } else {
-        println!("Command did not start");
+        colors::error_logger("command not found".to_string());
     }
 }
 
@@ -46,4 +48,19 @@ fn change_dir(new_path: &str) {
         Err(e)  => println!("Failed to change directory\n{}", e),
         _ => {}
     }
+}
+
+fn print_prompt() {
+    let path = env::current_dir().unwrap(); 
+    println!("{}{} >> RUSHING IN {}{}", colors::ANSI_BOLD, colors::ANSI_COLOR_CYAN, path.display(), colors::RESET);
+}
+
+fn tokenize(command_string: &mut String) -> Vec<Vec<&str>> {
+    command_string.pop();
+    let commands: Vec<&str> = command_string.split(',').collect();
+    let mut command_tokens: Vec<Vec<&str>> = Vec::new();
+    for command in commands.iter() {
+        command_tokens.push(command.split(' ').collect());
+    }
+    command_tokens
 }
